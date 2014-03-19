72352efaab557a84e1ccc5930a7835952e510a71
Anthony Odubela <aodubela@yahoo-inc.com>
2014-03-18 22:24:10 -0700

    initial check in for actnow android app

---
+++ src/org/actnow/SignInActivity.java
@@ -0,0 +1,13 @@
+package org.actnow;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class SignInActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fragment_login);
+	}
+}
